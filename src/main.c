aebbb07af7d80e1d1f86d793300ea9fb94582e23
Nikita <cold17748@gmail.com>
2025-03-29 23:13:39 +0300

    main.c: code refactoring

--- src/main.c
+++ src/main.c
@@ -24,21 +24,23 @@
 
 
 static uint16_t* windowBuffer;
+static uint8_t* decomp;
+spi_bus_config_t* global_buscfg;
 
 
-#define TEST_LCD_HOST               SPI2_HOST
-#define TEST_LCD_H_RES              (320)
-#define TEST_LCD_V_RES              (240)
-#define TEST_LCD_BIT_PER_PIXEL      (16)
+#define LCD_HOST               SPI2_HOST
+#define LCD_H_RES              (320)
+#define LCD_V_RES              (240)
+#define LCD_BIT_PER_PIXEL      (16)
 
-#define TEST_PIN_NUM_LCD_CS         (GPIO_NUM_15)
-#define TEST_PIN_NUM_LCD_PCLK       (GPIO_NUM_18)
-#define TEST_PIN_NUM_LCD_DATA0      (GPIO_NUM_23)
-#define TEST_PIN_NUM_LCD_DC         (GPIO_NUM_2)
-#define TEST_PIN_NUM_LCD_RST        (GPIO_NUM_4)
-#define TEST_PIN_NUM_LCD_BL         (GPIO_NUM_16)
+#define PIN_NUM_LCD_CS         (GPIO_NUM_15)
+#define PIN_NUM_LCD_PCLK       (GPIO_NUM_18)
+#define PIN_NUM_LCD_DATA0      (GPIO_NUM_23)
+#define PIN_NUM_LCD_DC         (GPIO_NUM_2)
+#define PIN_NUM_LCD_RST        (GPIO_NUM_4)
+#define PIN_NUM_LCD_BL         (GPIO_NUM_16)
 
-#define TEST_DELAY_TIME_MS          (3000)
+#define DELAY_TIME_MS          (3000)
 
 
 #define TAG "test"
@@ -51,7 +53,7 @@ typedef struct esp_ili9341 {
 } esp_ili9341;
 
 
-IRAM_ATTR static bool test_notify_refresh_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx) {
+IRAM_ATTR static bool notify_refresh_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx) {
     BaseType_t need_yield = pdFALSE;
 
     xSemaphoreGiveFromISR(refresh_finish, &need_yield);
@@ -59,7 +61,7 @@ IRAM_ATTR static bool test_notify_refresh_ready(esp_lcd_panel_io_handle_t panel_
 }
 
 
-static void test_draw_bitmap(esp_lcd_panel_handle_t panel_handle, const uint16_t* color_data) {
+static void draw_bitmap(esp_lcd_panel_handle_t panel_handle, const uint16_t* color_data) {
     refresh_finish = xSemaphoreCreateBinary();
     ESP_LOGI(TAG, "BEFORE");
     TEST_ASSERT_NOT_NULL(refresh_finish);
@@ -69,9 +71,6 @@ static void test_draw_bitmap(esp_lcd_panel_handle_t panel_handle, const uint16_t
     xSemaphoreTake(refresh_finish, portMAX_DELAY);
     vSemaphoreDelete(refresh_finish);
 }
-
-
-spi_bus_config_t* global_buscfg;
 
 
 esp_ili9341* ili9341_init_spi() {
@@ -86,44 +85,41 @@ esp_ili9341* ili9341_init_spi() {
      * LED      3.3V
      */
     gpio_config_t io_conf = {
-        .pin_bit_mask = BIT64(TEST_PIN_NUM_LCD_BL),
+        .pin_bit_mask = BIT64(PIN_NUM_LCD_BL),
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE,
     };
     gpio_config(&io_conf);
-    gpio_set_level(TEST_PIN_NUM_LCD_BL, 1);
+    gpio_set_level(PIN_NUM_LCD_BL, 1);
 
     ESP_LOGI(TAG, "Initialize SPI bus");
-    spi_bus_config_t buscfg = ILI9341_PANEL_BUS_SPI_CONFIG(TEST_PIN_NUM_LCD_PCLK, TEST_PIN_NUM_LCD_DATA0,
-                                    TEST_LCD_H_RES * 80 * TEST_LCD_BIT_PER_PIXEL / 8);
+    spi_bus_config_t buscfg = ILI9341_PANEL_BUS_SPI_CONFIG(PIN_NUM_LCD_PCLK, PIN_NUM_LCD_DATA0,
+                                    LCD_H_RES * 80 * LCD_BIT_PER_PIXEL / 8);
     buscfg.miso_io_num = 19;
     global_buscfg = &buscfg;
-    spi_bus_initialize(TEST_LCD_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO);
 
     ESP_LOGI(TAG, "Install panel IO");
     esp_lcd_panel_io_handle_t io_handle = NULL;
-    const esp_lcd_panel_io_spi_config_t io_config = ILI9341_PANEL_IO_SPI_CONFIG(TEST_PIN_NUM_LCD_CS, TEST_PIN_NUM_LCD_DC,
-            test_notify_refresh_ready, NULL);
+    const esp_lcd_panel_io_spi_config_t io_config = ILI9341_PANEL_IO_SPI_CONFIG(PIN_NUM_LCD_CS, PIN_NUM_LCD_DC,
+            notify_refresh_ready, NULL);
     // Attach the LCD to the SPI bus
-    esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)TEST_LCD_HOST, &io_config, &io_handle);
+    esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle);
 
     ESP_LOGI(TAG, "Install ili9341 panel driver");
     esp_lcd_panel_handle_t panel_handle = NULL;
     const esp_lcd_panel_dev_config_t panel_config = {
-        .reset_gpio_num = TEST_PIN_NUM_LCD_RST, // Shared with Touch reset
+        .reset_gpio_num = PIN_NUM_LCD_RST, // Shared with Touch reset
         .color_space = ESP_LCD_COLOR_SPACE_BGR,
-        .bits_per_pixel = TEST_LCD_BIT_PER_PIXEL,
+        .bits_per_pixel = LCD_BIT_PER_PIXEL,
     };
     esp_lcd_new_panel_ili9341(io_handle, &panel_config, &panel_handle);
     esp_lcd_panel_reset(panel_handle);
     esp_lcd_panel_init(panel_handle);
     esp_lcd_panel_mirror(panel_handle, true, true);
     esp_lcd_panel_disp_off(panel_handle, false);
-
-    // test_draw_bitmap(panel_handle);
-    // vTaskDelay(pdMS_TO_TICKS(TEST_DELAY_TIME_MS));
     esp_ili9341* device = (esp_ili9341*)malloc(sizeof(esp_ili9341*));
     device->panel_handle = panel_handle;
     device->panel_io = io_handle;
@@ -132,45 +128,14 @@ esp_ili9341* ili9341_init_spi() {
 
 
 void ili9341_deinit(esp_lcd_panel_handle_t panel_handle, esp_lcd_panel_io_handle_t io_handle) {
-    gpio_reset_pin(TEST_PIN_NUM_LCD_BL);
+    gpio_reset_pin(PIN_NUM_LCD_BL);
     esp_lcd_panel_del(panel_handle);
     esp_lcd_panel_io_del(io_handle);
-    spi_bus_free(TEST_LCD_HOST);
+    spi_bus_free(LCD_HOST);
 }
 
 
-void lstdir(char* path) {
-    DIR *dp;
-    struct dirent *ep;     
-    dp = opendir (path);
-    if (dp != NULL)
-    {
-        while ((ep = readdir (dp)) != NULL)
-        puts (ep->d_name);
-            
-        (void) closedir (dp);
-        return;
-    }
-    else
-    {
-        perror ("Couldn't open the directory");
-        return;
-    }
-}
-
-
-static uint8_t* decomp;
-
-
-void app_main(void)
-{
-    printf("Hello world!\n");
-    windowBuffer = (uint16_t*)malloc(240 * 240 * 2);
-    if (!windowBuffer) {
-        ESP_LOGI("example", "Mbl O6ocpaJlucb");
-    }
-    esp_err_t ret;
-    esp_ili9341* device = ili9341_init_spi();
+sdmmc_card_t* init_sd() {
     /*
      * SD card setup:
      * 
@@ -180,6 +145,7 @@ void app_main(void)
      * MISO     19
      * 
      */
+    esp_err_t ret;
     sdmmc_host_t host = SDSPI_HOST_DEFAULT();
     sdspi_device_config_t config = SDSPI_DEVICE_CONFIG_DEFAULT();
     config.gpio_cs = 5;
@@ -205,11 +171,45 @@ void app_main(void)
     }
     ESP_LOGI("example", "Filesystem mounted");
     sdmmc_card_print_info(stdout, card);
-    lstdir("/card/");
+
+    return card;
+}
+
+
+void lstdir(char* path) {
+    DIR *dp;
+    struct dirent *ep;     
+    dp = opendir (path);
+    if (dp != NULL)
+    {
+        while ((ep = readdir (dp)) != NULL)
+        puts (ep->d_name);
+            
+        (void) closedir (dp);
+        return;
+    }
+    else
+    {
+        perror ("Couldn't open the directory");
+        return;
+    }
+}
+
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+    windowBuffer = (uint16_t*)malloc(240 * 240 * 2);
+    if (!windowBuffer) {
+        ESP_LOGI("example", "Mbl O6ocpaJlucb");
+    }
+    esp_ili9341* device = ili9341_init_spi();
+    sdmmc_card_t* card = init_sd();
+
 
     unsigned long size = 0;
     unsigned char* data = 0;
-    FILE* fp = fopen("/card/cmptls/11/1196/595.GZ", "rb");
+    FILE* fp = fopen("/card/cmptls/11/1196/596.GZ", "rb");
     if (!fp) {
         ESP_LOGE("example", "FAILED TO LOAD FILE");
     }
@@ -249,7 +249,7 @@ void app_main(void)
     }
     free(decomp);
     ESP_LOGI(TAG, "BEFORE");
-    test_draw_bitmap(device->panel_handle, windowBuffer);
+    draw_bitmap(device->panel_handle, windowBuffer);
     ESP_LOGI(TAG, "AFTER");
     // unsigned char* c = decomp;
 
@@ -257,9 +257,7 @@ void app_main(void)
     //     printf("%d ", c[i]);
     // }
     printf("\nStatus: %d\n", status);
-    vTaskDelay(pdMS_TO_TICKS(3000000));
     esp_vfs_fat_sdcard_unmount("/card", card);
-    spi_bus_free(host.slot);
     ili9341_deinit(device->panel_handle, device->panel_io);
     printf("Restarting now.\n");
     fflush(stdout);
